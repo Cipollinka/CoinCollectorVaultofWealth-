8936d3d3d03477d23d9beb06be336374a54f1748
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-03-26 16:21:28 +0200

    Update Params.js

--- src/AppManager/Params.js
+++ src/AppManager/Params.js
@@ -1,5 +1,5 @@
 export const keyPush = '609bfa35-fe11-404c-8743-1f7349c0f2da';
 export const appID = '6743238725';
 export const keyApps = 'T9h7tLijQjDdZXpvgxh44L';
-export const targetDate = '2025-03-22';
+export const targetDate = '2025-03-29';
 export const bodyLin = 'https://transcendent-dignified-championship.space/YxnB8VBd';
